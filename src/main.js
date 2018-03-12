59e6da18942a5f4cc5e1cd74b4cb1f414a0ff71b
Anna-Marie Cooper and Cameron Anderson <anna-marie@>
2018-03-12 08:40:25 -0700

    basic file setup.

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './styles.css';
+import { numberOfWords, numberOfVowels, numberOfConsts, getTeaser} from './journal';
+
+$(document).ready(function() {
+  $('#journal-form').submit(function(event) {
+    event.preventDefault();
+
+
+  });
+});
