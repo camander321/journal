5991c7762e3c0545f59f890a7d17011828cf1be2
Anna-Marie Cooper and Cameron Anderson <cameron@>
2018-03-12 09:28:24 -0700

    all functions working.:

---
+++ src/journal.js
@@ -0,0 +1,33 @@
+export function numberOfWords (entry) {
+  return entry.split(" ").length;
+}
+
+export function numberOfVowels(entry) {
+  var count = 0;
+
+  for (var i = 0; i < entry.length; i++) {
+    if ("aeiouAEIOU".includes(entry[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+export function numberOfConsts(entry) {
+  var count = 0;
+
+  for (var i = 0; i < entry.length; i++) {
+    if (!"aeiouAEIOU ".includes(entry[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+export function getTeaser(entry) {
+  var words = entry.split(" ");
+  words.splice(8);
+  return words.join(' ');
+}
